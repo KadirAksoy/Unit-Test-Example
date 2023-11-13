a123b35ecde05245253fd3425b37059aaa758232
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-11-13 18:41:44 +0300

    Initial commit

---
+++ src/test/java/com/kadiraksoy/UnitTestExample/UnitTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.UnitTestExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
